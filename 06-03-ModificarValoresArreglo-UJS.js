cbb5fb1c268571d834915dbc4d6cae4e69058fe4
Danielofo83 <42302439+Danielofo83@users.noreply.github.com>
2023-07-19 11:20:51 -0600

    add
    add

--- 06-03-ModificarValoresArreglo-UJS.js
+++ 06-03-ModificarValoresArreglo-UJS.js
@@ -12,7 +12,8 @@ for(let i = 0; i < autos.length; i++){
 autos[1] = 'MerecedesBenz';
 console.log(autos[1]);
 
+//estamos imprimiendo los elementos que tenemos en un arreglo
+console.log(autos.length);
 //con esta funcion agregamos un nuevo elemento al final del arreglo con el valor de Audi
-
-autos.push('Audi');
+ autos.push('Audi');
 console.log(autos);
